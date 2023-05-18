ce053d5f3cd49aecc59f904d97c4a58942730fb2
Gaurav Sarma <gauravsarma1992@gmail.com>
2023-05-18 14:57:37 +0530

    Updating git configs

--- git-aliases.sh
+++ git-aliases.sh
@@ -1,10 +1,19 @@
 alias gic='git commit --no-verify'
+alias gich='git checkout'
 alias gis='git status'
 alias gil='git log'
 alias gib='git branch'
 alias gia='git add'
 alias gid='git diff'
 alias gidc='git diff --cached'
-alias gipl='git pull -r origin'
-alias gips='git push origin'
+
+# Default branch: master/main 
+alias giplm="git pull -r origin $(git rev-parse --abbrev-ref origin/HEAD | awk -F/ '{print $2}')"
+alias gipsm="git push origin $(git rev-parse --abbrev-ref origin/HEAD | awk -F/ '{print $2}')"
+
+# Current branch
+alias gipl="git pull -r origin $(git rev-parse --abbrev-ref HEAD)"
+alias gips="git push origin $(git rev-parse --abbrev-ref HEAD)"
+
+alias gir='git reset'
 alias pr_create="gh pr create"
